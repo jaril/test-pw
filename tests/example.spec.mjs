a2e97710185f86907f7b2f46149bb907b0b291eb
Jaril <jarilvalenciano@gmail.com>
2022-05-17 06:17:03 -0700

    Add a second test within example.spec.mjs

--- tests/example.spec.mjs
+++ tests/example.spec.mjs
@@ -1,6 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-test('basic test', async ({ page }) => {
+test('basic test 1', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  const title = page.locator('.navbar__inner .navbar__title');
+  await expect(title).toHaveText('Playwright');
+});
+test('basic test 2', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   const title = page.locator('.navbar__inner .navbar__title');
   await expect(title).toHaveText('Playwright');
