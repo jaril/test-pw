383630aa559ccbc154ed8f92648c76819b6e5fb1
Jaril <jarilvalenciano@gmail.com>
2022-06-08 12:36:21 -0700

    Add new tests

---
+++ tests/breakpoints-01.spec.mjs
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test('breakpoints should have basic functionality (10% pass)', async ({ page }) => {
+
+  await page.goto('https://playwright.dev/');
+  const pass = Math.random() > 0.9;
+  await expect(pass).toBe(true);
+});
+test('breakpoints should not diverge (20% pass', async ({ page }) => {
+
+  await page.goto('https://playwright.dev/');
+  const pass = Math.random() > 0.8;
+  await expect(pass).toBe(true);
+});
+test('breakpoints should support catch, finally, generators, and async/await (30% pass)', async ({ page }) => {
+
+  await page.goto('https://playwright.dev/');
+  const pass = Math.random() > 0.7;
+  await expect(pass).toBe(true);
+});
